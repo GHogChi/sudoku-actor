ba4311a66d8ea82aaf8970c794909cd166aded3a
GHogChi <rossentj@acm.org>
2016-03-11 01:10:05 -0600

    Adding Cell class and test stubs

---
+++ src/main/scala/Cell.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by tomcat on 3/11/16.
+  */
+case class Cell() {
+
+}
---
+++ src/test/scala/WhenCellStateChanges.scala
@@ -0,0 +1,13 @@
+import org.scalatest.FunSpec
+
+/**
+  * Created by tomcat on 3/11/16.
+  */
+class WhenCellStateChanges extends FunSpec {
+
+  describe("something"){
+    it("does something"){
+      val cell = new Cell()
+    }
+  }
+}
